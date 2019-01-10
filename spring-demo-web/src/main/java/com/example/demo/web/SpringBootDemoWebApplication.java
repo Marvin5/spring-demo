e586757dd1c4b70f48021d09facbd12029e7e5d4
marvin <iamfmb@foxmail.com>
2019-01-10 22:41:30 +0800

    Fixing web/job build can not include core problem

--- spring-demo-web/src/main/java/com/example/demo/web/SpringBootDemoWebApplication.java
+++ spring-demo-web/src/main/java/com/example/demo/web/SpringBootDemoWebApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo.web;
 
+import com.example.demo.core.entity.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
