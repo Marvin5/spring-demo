7ff6f8430433b0d1e5c503c62e3fb5ba6cced7d4
Marvin Fang <marvin.fang@cargosmart.com>
2019-04-02 16:49:45 +0800

    restdoc

---
+++ spring-demo-web/src/test/java/com/example/demo/web/config/CustomizationConfiguration.java
@@ -0,0 +1,14 @@
+package com.example.demo.web.config;
+
+import org.springframework.boot.test.autoconfigure.restdocs.RestDocsMockMvcConfigurationCustomizer;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentationConfigurer;
+import org.springframework.restdocs.templates.TemplateFormats;
+
+@TestConfiguration
+public class CustomizationConfiguration implements RestDocsMockMvcConfigurationCustomizer {
+  @Override
+  public void customize(MockMvcRestDocumentationConfigurer configurer) {
+    configurer.snippets().withTemplateFormat(TemplateFormats.markdown());
+  }
+}
--- spring-demo-web/src/test/java/com/example/demo/web/controller/EchoControllerTest.java
+++ spring-demo-web/src/test/java/com/example/demo/web/controller/EchoControllerTest.java
@@ -5,19 +5,26 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.restdocs.RestDocsMockMvcConfigurationCustomizer;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentationConfigurer;
+import org.springframework.restdocs.templates.TemplateFormats;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
+@ExtendWith({SpringExtension.class, RestDocumentationExtension.class})
 @WebMvcTest(EchoController.class)
+@AutoConfigureRestDocs
 @WithMockUser
 public class EchoControllerTest {
   @Autowired private MockMvc mockMvc;
@@ -27,9 +34,19 @@ public class EchoControllerTest {
   @Test
   @Tag("fast")
   public void testPubGet() throws Exception {
-    doNothing().when(echoService).echo();
+    // doNothing().when(echoService).echo();
     this.mockMvc
         .perform(get("/api/pub/echo").accept(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk());
+        .andExpect(status().is2xxSuccessful())
+        .andDo(MockMvcRestDocumentation.document("pubGet"));
   }
+
+  @TestConfiguration
+  public static class CustomizationConfiguration implements RestDocsMockMvcConfigurationCustomizer {
+    @Override
+    public void customize(MockMvcRestDocumentationConfigurer configurer) {
+      configurer.snippets().withTemplateFormat(TemplateFormats.markdown());
+    }
+  }
+
 }
